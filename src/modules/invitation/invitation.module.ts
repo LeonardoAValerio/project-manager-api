9f6fa340dc5484312ee2df91874999aa17006e52
LeonardoAValerio <leonardoaugustovalerio@gmail.com>
2025-01-27 23:05:37 -0300

    fix: dependencies of modules

--- src/modules/invitation/invitation.module.ts
+++ src/modules/invitation/invitation.module.ts
@@ -2,12 +2,14 @@ import { Module } from "@nestjs/common";
 import { PrismaService } from "src/shared/database/prisma.service";
 import { InvitationRepositorie } from "./invitation.repositorie";
 import { InvitationService } from "./invitation.service";
+import { JwtService } from "@nestjs/jwt";
 
 @Module({
     providers: [
         PrismaService,
         InvitationRepositorie,
-        InvitationService
+        InvitationService,
+        JwtService
     ],
     exports: [
         InvitationService
--- src/modules/project/project.module.ts
+++ src/modules/project/project.module.ts
@@ -4,11 +4,13 @@ import { ProjectRepositorie } from "./project.repositorie";
 import { ProjectService } from "./project.service";
 import { Module } from "@nestjs/common";
 import { ColaboratorService } from "../colaborator/colaborator.service";
-import { ColaboratorModule } from "../colaborator/colaborator.module";
 import { ColaboratorRepositorie } from "../colaborator/colaborator.repositore";
 import { EmailService } from "src/shared/utils/email/email.service";
+import { UserModule } from "../user/user.module";
+import { InvitationModule } from "../invitation/invitation.module";
 
 @Module({
+    imports: [UserModule, InvitationModule],
     controllers: [ProjectController],
     providers: [
         ProjectRepositorie, 
