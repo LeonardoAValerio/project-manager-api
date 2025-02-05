5e3814ae28716b968f98472f6bc7300f83f535a5
LeonardoAValerio <leonardoaugustovalerio@gmail.com>
2025-02-04 23:05:48 -0300

    feat: create GetProfile

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Delete, Get, Param, Post, Request, UseGuards } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Req, UseGuards } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { CreateUserDto } from "./dto/create-user.dto";
-import { ApiBadRequestResponse, ApiCreatedResponse, ApiOkResponse, ApiParam } from "@nestjs/swagger";
+import { ApiBadRequestResponse, ApiBearerAuth, ApiCreatedResponse, ApiOkResponse, ApiParam } from "@nestjs/swagger";
 import { ValidationErrorResponse } from "src/shared/utils/responses/validation-error.response";
 import { GetUserDto } from "./dto/get-user.dto";
 import { AuthGuard } from "@nestjs/passport";
+import { Request } from "express";
 
 @Controller("user")
 export class UserController {
@@ -49,5 +50,17 @@ export class UserController {
     @Delete(":id_user")
     async delete(@Param("id_user") id: string) {
         await this.userService.deleteById(id);
+    }
+
+    @ApiOkResponse({
+        description: "Get your profile.",
+        type: GetUserDto
+    })
+    @ApiBearerAuth("authorization")
+    @UseGuards(AuthGuard("jwt"))
+    @Get("profile")
+    async profile(@Req() req: Request) {
+        const { name, email, id} = req.user;
+        return {id, name, email}
     }
 }
